2bcc069734a7c26d80a6986624af0eaaee657bf0
adinath7l <adinathlahane2002@gmail.com>
2025-01-15 21:07:21 +0530

    Update settings.py

--- unified_project/settings.py
+++ unified_project/settings.py
@@ -1,4 +1,4 @@
-ALLOWED_HOSTS = ['adinath7l.pythonanywhere.com']
+ALLOWED_HOSTS = ['Adinath7l.pythonanywhere.com']
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
